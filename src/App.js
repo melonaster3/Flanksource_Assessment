e03c5777206c25690773b1e19e1f0aded277d193
Sang Lee <melonaster@gmail.com>
2022-08-30 06:38:04 +0000

    Got all teams into a single array list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import "./styles.css";
 import {data} from "./data.js"
 export default function App() {
   console.log(data);
-  
+  let teamList = [];
+  for(let games of data) {
+    let teams = Object.keys(games.score)
+    teams.forEach( team => {
+      if (!teamList.includes(team)) {
+        teamList.push(team);
+      }
+    })
+  }
+  console.log(teamList)
+
   return (
     <div className="App">
       <h1>Hello Codesandbox</h1>
