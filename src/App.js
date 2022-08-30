c847d3ecd3d6ade07d83c6cddc44dc0527de7a19
Sang Lee <melonaster@gmail.com>
2022-08-30 07:57:35 +0000

    Got all points in a single object

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import "./styles.css";
 import {data} from "./data.js"
 export default function App() {
-  console.log(data);
   let teamList = [];
-  for(let games of data) {
-    let teams = Object.keys(games.score)
-    teams.forEach( team => {
+  let pointList = {};
+  for (let games of data) {
+    let teams = Object.keys(games.score);
+    teams.forEach(team => {
       if (!teamList.includes(team)) {
+        pointList[team] = 0;
         teamList.push(team);
       }
+      if (games.score[team] === null) {
+        games.score[team] = 0;
+        
+      }
     })
+
+    if(games.score[teams[0]] > games.score[teams[1]] ) {
+      pointList[teams[0]] += 3;
+    }
+
+    if(games.score[teams[1]] > games.score[teams[0]] ) {
+      pointList[teams[1]] +=3;
+    }
+    
+    if(games.score[teams[0]] === games.score[teams[1]]){
+      pointList[teams[0]] +=1;
+      pointList[teams[1]] +=1;
+    }
+    
   }
-  console.log(teamList)
+  
+  console.log(pointList);
 
   return (
     <div className="App">
