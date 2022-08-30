5af4fba52563feaa3cea09c1942b45891cd82b00
Sang Lee <melonaster@gmail.com>
2022-08-30 06:23:12 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./styles.css";
-
+import {data} from "./data.js"
 export default function App() {
+  console.log(data);
+  
   return (
     <div className="App">
       <h1>Hello Codesandbox</h1>
