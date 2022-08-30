170ec328f705e0fd767e1d6a6ea97096ec5dcddc
Sang Lee <melonaster@gmail.com>
2022-08-30 08:08:12 +0000

    Stored all team info in one object

--- src/App.js
+++ src/App.js
@@ -2,36 +2,62 @@ import "./styles.css";
 import {data} from "./data.js"
 export default function App() {
   let teamList = [];
-  let pointList = {};
+  let teamInfo = {};
   for (let games of data) {
     let teams = Object.keys(games.score);
     teams.forEach(team => {
       if (!teamList.includes(team)) {
-        pointList[team] = 0;
+        teamInfo[team] = {
+          points : 0,
+          games : 0, 
+          goalScored : 0,
+          goalConceded :0
+        }
         teamList.push(team);
       }
       if (games.score[team] === null) {
         games.score[team] = 0;
-        
       }
     })
 
     if(games.score[teams[0]] > games.score[teams[1]] ) {
-      pointList[teams[0]] += 3;
+      teamInfo[teams[0]].points +=3; 
+      teamInfo[teams[0]].games ++;
+      teamInfo[teams[0]].goalScored += games.score[teams[0]];
+      teamInfo[teams[0]].goalConceded += games.score[teams[1]];
+
+      teamInfo[teams[1]].games ++;
+      teamInfo[teams[1]].goalScored += games.score[teams[1]];
+      teamInfo[teams[1]].goalConceded += games.score[teams[0]];
+
     }
 
     if(games.score[teams[1]] > games.score[teams[0]] ) {
-      pointList[teams[1]] +=3;
+      teamInfo[teams[1]].points +=3; 
+      teamInfo[teams[1]].games ++;
+      teamInfo[teams[1]].goalScored += games.score[teams[1]];
+      teamInfo[teams[1]].goalConceded += games.score[teams[0]];
+
+      teamInfo[teams[0]].games ++;
+      teamInfo[teams[0]].goalScored += games.score[teams[0]];
+      teamInfo[teams[0]].goalConceded += games.score[teams[1]];
     }
     
-    if(games.score[teams[0]] === games.score[teams[1]]){
-      pointList[teams[0]] +=1;
-      pointList[teams[1]] +=1;
+    if(games.score[teams[1]] === games.score[teams[0]] ) {
+      teamInfo[teams[0]].points ++; 
+      teamInfo[teams[0]].games ++;
+      teamInfo[teams[0]].goalScored += games.score[teams[0]];
+      teamInfo[teams[0]].goalConceded += games.score[teams[1]];
+
+      teamInfo[teams[1]].points ++; 
+      teamInfo[teams[1]].games ++;
+      teamInfo[teams[1]].goalScored += games.score[teams[1]];
+      teamInfo[teams[1]].goalConceded += games.score[teams[0]];
     }
-    
+  
   }
   
-  console.log(pointList);
+  console.log(teamInfo)
 
   return (
     <div className="App">
